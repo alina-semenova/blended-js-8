a69e96103b7b2125f58e6c2f4aae3303fa7310b2
Alina Semenova <3alina5@gmail.com>
2025-11-05 22:12:58 +0200

    add code to test.js

---
+++ js/test.js
@@ -0,0 +1,74 @@
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index];
+// }
+
+// storage.tolowerCase();
+
+// function getCommonElements(lot1, lot2) {
+//   let lot3 = 0;
+//   for (const checkLot of lot1) {
+//   }
+//   return lot3;
+// }
+
+// function calculateTotalPrice(order) {
+//   let sumNumbers = 0;
+//   for (const number of order) {
+//     sumNumbers += number;
+//   }
+//   return sumNumbers;
+// }
+
+// function multiply() {
+//   let total = 1;
+
+//   for (const arg of arguments) {
+//     total *= arg;
+//   }
+
+//   return total;
+// }
+
+// console.log(multiply(1, 2, 3)); //  6
+// console.log(multiply(1, 2, 3, 4)); //  24
+// console.log(multiply(1, 2, 3, 4, 5)); //  120
+
+// const fruits = [
+//   'banana',
+//   'apple',
+//   'orange',
+//   'watermelon',
+//   'apple',
+//   'orange',
+//   'grape',
+//   'apple',
+// ];
+// const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
+// console.log(randomFruit);
+
+// function greet() {
+//   console.log(`Hello`);
+// }
+
+// greet('Jacob'); // "Hello, Jacob!"
+// greet(); // "Hello, Guest!"
+
+// function count(from, to, step = 1) {
+//   console.log(`${from}, ${to}, ${step}`);
+
+//   for (let i = from; i <= to; i += step) {
+//     // ...
+//   }
+// }
+
+// count(1, 15, 4); // "from: 1, to: 15, step: 4"
+// count(1, 15); // "from: 1, to: 15, step: 1"
+
+// function fn1(param1, param2 = 10) {
+//   console.log(param1);
+//   // console.log(param2);
+
+//   return true;
+// }
+
+// const res = fn1(2);
